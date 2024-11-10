409f906da745b2d76864fe0b4384caaef6a4046a
n.privezentsev <n.privezentsev@mail.ru>
2024-11-10 19:08:55 +0300

    Add initial code 6

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,5 +10,5 @@ class FoodgramAPITestCase(TestCase):
 
     def test_list_exists(self):
         """Проверка доступности списка рецептов."""
-        response = self.guest_client.get('/recipes/')
+        response = self.guest_client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
